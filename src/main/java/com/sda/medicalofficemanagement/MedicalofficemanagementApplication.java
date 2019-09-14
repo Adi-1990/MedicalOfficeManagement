5b988384f57af4abd1b047282a98f47a95cbf037
Adi <ady_de_bocy@yahoo.com>
2019-09-14 15:03:21 +0300

    first commit

---
+++ src/main/java/com/sda/medicalofficemanagement/MedicalofficemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.sda.medicalofficemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalofficemanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalofficemanagementApplication.class, args);
+    }
+
+}
